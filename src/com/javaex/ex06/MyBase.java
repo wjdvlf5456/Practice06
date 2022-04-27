550b40faeddf71b65298e0c4fa79f00062e3ebf7
최정필 <wjdvlf1407@gmail.com>
2022-04-27 21:57:00 +0900

    Ex06 작성중

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -3,5 +3,15 @@ package com.javaex.ex06;
 public class MyBase extends Base{
 
     //코드작성
+	//낮,밤,오후가 순서대로 오니 그에 맞게 작성한다. 
+	public void service(String state) {
+		
+		
+	}
+	
+	public void afternoon(){
+		System.out.println("오후도");
+		
+	}	
     
 }
