ec9516c10fdce26bdf3e70a73d0240248f803791
최정필 <wjdvlf1407@gmail.com>
2022-04-27 21:52:55 +0900

    Ex07 추가

--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -1,27 +1,26 @@
 package com.javaex.ex07;
 
 public class SmartPhone extends MusicPhone {
-    
-    public void execute(String str){
-        
-    //코드작성
-        if("앱".equals(str)){
-           	call();
-        }else{
-            super.execute(str);
-        }
-    }
- 
-    //메소드작성
-    public void call() {
-    	System.out.println("앱실행");
-    }
-    
-    //메소드작성
-    public void playMusic() {
-    	super.playMusic();
 
-    }
-    
-    
+	public void execute(String str) {
+
+		// 코드작성
+		if ("앱".equals(str)) {
+			call();
+		} else {
+			super.execute(str);
+		}
+	}
+
+	// 메소드작성
+	public void playMusic() {
+		super.playMusic();
+
+	}
+
+	// 메소드작성
+	public void call() {
+		System.out.println("앱실행");
+	}
+
 }
