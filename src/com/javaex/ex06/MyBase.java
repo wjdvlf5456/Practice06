deb44ef91197e55be45b7d7d054dec7b8238cf76
최정필 <wjdvlf1407@gmail.com>
2022-04-27 22:16:08 +0900

    Ex06 문제풀기

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -1,17 +1,27 @@
 package com.javaex.ex06;
 
-public class MyBase extends Base{
+public class MyBase extends Base {
 
-    //코드작성
-	//낮,밤,오후가 순서대로 오니 그에 맞게 작성한다. 
+	// 코드작성
+	// 낮,밤,오후가 순서대로 오니 그에 맞게 작성한다.
 	public void service(String state) {
-		
-		
+		if (state.equals("오후")) {
+			afternoon();
+		} else if (state.equals("낮")) {
+			day();
+		} else {
+			night();
+
+		}
+
 	}
-	
-	public void afternoon(){
-		System.out.println("오후도");
-		
-	}	
-    
+	public void day() {
+		System.out.println("낮에는 열심히 수업듣자");
+	}
+
+	public void afternoon() {
+		System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+
+	}
+
 }
