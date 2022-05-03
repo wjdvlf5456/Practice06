abae46344f0c592e14b91f512c61ed3f80df0438
최정필 <wjdvlf1407@gmail.com>
2022-05-03 17:00:36 +0900

    Ex04

--- src/com/javaex/ex04/UserApp.java
+++ src/com/javaex/ex04/UserApp.java
@@ -3,30 +3,23 @@ package com.javaex.ex04;
 public class UserApp {
 
 	public static void main(String[] args) {
-		
+
 		User jws = new Customer("jws", "j1234", "정우성", 1000);
-		
+
 		User yjs = new Customer("yjs", "y2345", "이효리", 2000);
-		
+
 		User master = new Employee("master", "m7788", "운영자", 5000000);
-		
+
 		User[] user = new User[3];
-		
+
 		user[0] = jws;
 		user[1] = yjs;
 		user[2] = master;
-		
+
 		for (int i = 0; i < user.length; i++) {
 			user[i].showInfo();
-			
+
 		}
-		
-		 
-		
-		
-		
-		
-		
 
 	}
 
